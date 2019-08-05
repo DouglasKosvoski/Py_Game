47fb5d6caa5936a2744939fa0cd14319318e0d09
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-08-05 08:46:35 -0300

    input for RGB colors

--- circle_colors.py
+++ circle_colors.py
@@ -6,26 +6,18 @@ pygame.init()
 canvas_width, canvas_height = 640, 480
 canvas = pygame.display.set_mode((canvas_width, canvas_height))
 
-white = (255,255,255)
-black = (0,0,0)
-gray  = (120,120,120)
-blue  = (0,0,255)
-green = (0,255,0)
-red   = (255,0,0)
+first_color = list(map(int, input("Enter RGB 1: ").split(',')))
+second_color = list(map(int, input("Enter RGB 2: ").split(',')))
+
+result_color = ((first_color[0] + second_color[0]) / 2,
+                (first_color[1] + second_color[1]) / 2,
+                (first_color[2] + second_color[2]) / 2)
 
 
 while True:
-    first_color = red
-    second_color = gray
-    result_color =  (first_color[0]+second_color[0])/2,(first_color[1]+second_color[1])/2,(first_color[2]+second_color[2])/2
-
     pygame.draw.circle(canvas, first_color,  (320-100,240), 100)
     pygame.draw.circle(canvas, second_color, (320+100,240), 100)
     pygame.draw.circle(canvas, result_color, (320,140), 100)
-
-    #print(result_color)
-    pygame.display.update()
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -35,3 +27,5 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 quit()
+
+    pygame.display.update()
